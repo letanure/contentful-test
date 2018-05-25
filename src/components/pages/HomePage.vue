ecfba2da00a8387159f322445152ed4c30a2b929
Luiz Tanure <letanure@gmail.com>
2018-05-25 03:55:14 +0200

    feat: load data from api

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -5,8 +5,8 @@
         TitleUi(:title='title')
 
     .columns.is-multiline
-      template(v-for='movie in movies')
-        .column.is-3
+      template(v-for='(movie, index) in movies', )
+        .column.is-3(:key='`movie-${index}`', )
           CardUi(:title='movie.name')
 </template>
 
@@ -24,24 +24,23 @@ export default {
   data () {
     return {
       title: 'Favorite Movies',
-      movies: [
-        {
-          name: 'aa',
-        },
-        {
-          name: 'bb',
-        },
-        {
-          name: 'cc',
-        },
-        {
-          name: 'dd',
-        },
-        {
-          name: 'ee',
-        },
-      ],
     }
+  },
+
+  computed: {
+    movies () {
+      return this.$store.state.movies.list
+    },
+  },
+
+  mounted () {
+    this.loadMovies()
+  },
+
+  methods: {
+    loadMovies () {
+      this.$store.dispatch('loadMovies')
+    },
   },
 }
 </script>
